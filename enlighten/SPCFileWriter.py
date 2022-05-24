e06ed478d443e679abd85df8a080bc3c31c76104
Evan Dort <edort@wasatchphotonics.onmicrosoft.com>
2022-05-24 08:36:47 -0400

    fix log pack endianess

--- enlighten/SPCFileWriter.py
+++ enlighten/SPCFileWriter.py
@@ -388,10 +388,10 @@ class SPCLog:
         mem_block = 4096 * round(block_size/4096)
         text_offset = 64 + data_len
         log.debug(f"log encoding values text len {text_len}, data_len {data_len}, block_size {block_size}, mem_block {mem_block}")
-        Bblock_size = pack(">l", block_size)
-        Bmem_size = pack(">l", mem_block)
-        Btext_offset = pack(">l", text_offset)
-        Bdata_len = pack(">l", data_len)
+        Bblock_size = pack("l", block_size)
+        Bmem_size = pack("l", mem_block)
+        Btext_offset = pack("l", text_offset)
+        Bdata_len = pack("l", data_len)
         Bdisk_len = b"\x00\x00\x00\x00"
         Breserved = bytes(bytearray(b"\x00"*LOG_RESERVE_LIMIT))
         return b"".join([Bblock_size, Bmem_size, Btext_offset, Bdata_len, Bdisk_len, Breserved])
