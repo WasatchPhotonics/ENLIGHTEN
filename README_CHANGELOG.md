dea03c8bfb25cf7ddfb72551a9322d9983e13f0c
rc14193 <edort@wasatchphotonics.onmicrosoft.com>
2022-09-28 08:19:56 -0400

    add tf hidden import, uprev and changelog

--- README_CHANGELOG.md
+++ README_CHANGELOG.md
@@ -2,6 +2,8 @@
 
 # 3.2 Open-Source
 
+- 2022-09-28 3.2.33
+    - change tensorflow to hidden import to speed up start
 - 2022-09-27 3.2.32
     - add warning dialogue for plugins
 - 2022-09-22 3.2.31
--- enlighten/common.py
+++ enlighten/common.py
@@ -12,7 +12,7 @@ application.
 @todo consider making "common" a package (directory), and each of these classes
       can be modules (files) within it
 """
-VERSION = "3.2.32"
+VERSION = "3.2.33"
 
 """ ENLIGHTEN's application version number (checked by scripts/deploy and bootstrap.bat) """
 
