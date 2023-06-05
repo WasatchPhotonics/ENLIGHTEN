559c3e3c1535c316de08dd59548ca3333a127041
Mark Zieg <mark@zieg.com>
2023-06-05 15:46:31 -0400

    removed debugs

--- enlighten/measurement/SaveOptions.py
+++ enlighten/measurement/SaveOptions.py
@@ -195,9 +195,7 @@ class SaveOptions():
         self.le_note            .setText   (re.sub(",", "", self.config.get(s, "note")))
 
     def init_checkbox(self, cb, option):
-        log.debug(f"init_checkbox (option {option})")
         value = self.config.get_bool("save", option)
-        log.debug(f"init_checkbox setting {value} to {cb}")
         cb.setChecked(value)
 
     ## Update widgets and config state when:
@@ -212,9 +210,7 @@ class SaveOptions():
         # decide which widgets should be ENABLED (not checked)
         ########################################################################
 
-        log.debug("update_widgets: checking save_csv")
         save_csv = self.save_csv()
-        log.debug(f"update_widgets: save_csv = {save_csv}")
 
         # these widgets are dependent on the CSV checkbox
         self.rb_by_row.setEnabled(save_csv)
@@ -248,7 +244,6 @@ class SaveOptions():
         # update config
         ########################################################################
 
-        log.debug(f"update_widgets: setting config from current checkbox state")
         s = "save"
         self.config.set(s, "order", "row" if self.save_by_row() else "col")
         self.config.set(s, "collated",           self.save_collated())
