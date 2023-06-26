6102bf845391da1415352ba76cb2777732f9350c
Mark Zieg <mark@zieg.com>
2023-06-26 10:29:50 -0400

    update Savitzky-Golay plugin to 'functional'

--- pluginExamples/Filters/SavitzkyGolay.py
+++ pluginExamples/Filters/SavitzkyGolay.py
@@ -3,32 +3,22 @@ import logging
 
 from scipy.signal import savgol_filter
 
-from EnlightenPlugin import EnlightenPluginBase,    \
-                            EnlightenPluginField,    \
-                            EnlightenPluginResponse,  \
-                            EnlightenPluginConfiguration
+from EnlightenPlugin import *
 
 log = logging.getLogger(__name__)
 
 class SavitzkyGolay(EnlightenPluginBase):
 
     def get_configuration(self):
-        fields = []
-        fields.append(EnlightenPluginField(name="Half-Length", datatype="int", minimum=1, maximum=50, initial=2, direction="input",
-            tooltip="Window will be n*2 - 1"))
-        fields.append(EnlightenPluginField(name="Poly Order", datatype="int", minimum=0, maximum=7, initial=1, direction="input",
-            tooltip="Polynomial order for the fit"))
-        fields.append(EnlightenPluginField(name="Deriv Order", datatype="int", minimum=0, maximum=7, initial=0, direction="input",
-            tooltip="Deriviative order (optional)"))
-        fields.append(EnlightenPluginField(name="Delta", datatype="float", minimum=0.1, maximum=10.0, initial=1.0, step=0.1, direction="input",
-            tooltip="Sample spacing (optional)"))
-        return EnlightenPluginConfiguration(
-            name = "Savitzky-Golay", 
-            fields = fields,
-            series_names = ["savgol"])
+        self.name = "Savitzky-Golay"
 
-    def connect(self, enlighten_info):
-        return super().connect(enlighten_info)
+        self.field(name="Half-Length", datatype="int",   minimum=1,   maximum=50,   initial=2,             direction="input", tooltip="Window will be n*2 - 1")
+        self.field(name="Poly Order",  datatype="int",   minimum=0,   maximum=7,    initial=1,             direction="input", tooltip="Polynomial order for the fit")
+        self.field(name="Deriv Order", datatype="int",   minimum=0,   maximum=7,    initial=0,             direction="input", tooltip="Deriviative order (optional)")
+        self.field(name="Delta",       datatype="float", minimum=0.1, maximum=10.0, initial=1.0, step=0.1, direction="input", tooltip="Sample spacing (optional)")
+
+        self.has_other_graph = False
+        self.block_enlighten = True 
 
     def process_request(self, request):
         win_len   = request.fields["Half-Length"] * 2 - 1
@@ -36,13 +26,11 @@ class SavitzkyGolay(EnlightenPluginBase):
         deriv     = request.fields["Deriv Order"]
         delta     = request.fields["Delta"]
 
-        savgol = savgol_filter(
-            request.processed_reading.processed, 
-            window_length = win_len,
-            polyorder     = polyorder,
-            deriv         = deriv,
-            delta         = delta)
-        return EnlightenPluginResponse(request, series = { "savgol": savgol })
+        smoothed = savgol_filter(
+            x             = request.processed_reading.processed, 
+            window_length = win_len,    # < len(x)
+            polyorder     = polyorder,  # < win_len
+            deriv         = deriv,      #  # >= 0
+            delta         = delta)      #  # spacing if delta>1, default 1
 
-    def disconnect(self):
-        super().disconnect()
+        return EnlightenPluginResponse(request, series = { "Savitzky-Golay": { "y": smoothed}})
