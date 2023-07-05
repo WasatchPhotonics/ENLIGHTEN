d8a91881f7493b76644efd5a08213976d3385636
Samie Bee <sbee@wasatchphotonics.com>
2023-07-05 16:26:35 -0400

    show multiple local baselines. Not incl pd frame, center, meta

--- pluginExamples/Analysis/LocalBaseline.py
+++ pluginExamples/Analysis/LocalBaseline.py
@@ -13,40 +13,43 @@ class LocalBaseline(EnlightenPluginBase):
     def get_configuration(self):
         self.name = "Baseline"
 
-        self.field(
-            name = "x",
-            initial = 1014, minimum = -10000, maximum = 10000, step = .2,
-            datatype = "float", direction = "input"
-        )
-        
-        self.field(
-            name = "Left",
-            initial = 10, minimum = 0, maximum = 10000, step = .2,
-            datatype = "float", direction = "input"
-        )        
-        
-        self.field(
-            name = "Right",
-            initial = 10, minimum = 0, maximum = 10000, step = .2,
-            datatype = "float", direction = "input"
-        )
+        self.count = 4
 
-        self.field(
-            name = "Center",
-            datatype = "button",
-            callback = self.center
-        )
+        for i in range(self.count):
+            self.field(
+                name = "x_"+str(i),
+                initial = 1014, minimum = -10000, maximum = 10000, step = .2,
+                datatype = "float", direction = "input"
+            )
+            
+            self.field(
+                name = "Left_"+str(i),
+                initial = 10, minimum = 0, maximum = 10000, step = .2,
+                datatype = "float", direction = "input"
+            )        
+            
+            self.field(
+                name = "Right_"+str(i),
+                initial = 10, minimum = 0, maximum = 10000, step = .2,
+                datatype = "float", direction = "input"
+            )
+
+            self.field(
+                name = "Center_"+str(i),
+                datatype = "button",
+                callback = lambda *k: self.center(i)
+            )
 
         self.has_other_graph = False
         self.block_enlighten = True # for metadata
 
-    def center(self):
+    def center(self, i):
 
         # PySide2.QtWidgets.QDoubleSpinBox
-        x_widget = self.get_widget_from_name("x")
+        x_widget = self.get_widget_from_name("x_"+str(i))
 
         x_pix = self.to_pixel(x_widget.value())
-        for i in range(100):
+        for j in range(100):
             x_pix_n = x_pix-1+max(enumerate(self.spectrum[x_pix-1:x_pix+1+1]), key=lambda P: P[1])[0]
             if x_pix_n == x_pix:
                 break
@@ -57,67 +60,68 @@ class LocalBaseline(EnlightenPluginBase):
         pr = request.processed_reading
         spectrum = pr.get_processed()
 
-        x = self.get_widget_from_name("x").value()
-        left = self.get_widget_from_name("Left").value()
-        right = self.get_widget_from_name("Right").value()
+        for i in range(self.count):
+            x = self.get_widget_from_name("x_"+str(i)).value()
+            left = self.get_widget_from_name("Left_"+str(i)).value()
+            right = self.get_widget_from_name("Right_"+str(i)).value()
 
-        start = x-left
-        end = x+right
-
-        self.plot(
-            title="x",
-            color="red",
-            x=[x, x],
-            y=[min(spectrum), max(spectrum)],
-        )
-
-        self.plot(
-            title="Range",
-            color="blue",
-            x=[start, start],
-            y=[min(spectrum), max(spectrum)],
-        )
-        self.plot(
-            color="blue",
-            x=[end, end],
-            y=[min(spectrum), max(spectrum)],
-        )
-
-        start_pixel = self.to_pixel(start)
-        end_pixel = self.to_pixel(end)
-
-        sub_spectrum = list(spectrum[start_pixel:end_pixel+1])
-        if sub_spectrum:
-
-            i = (x-start)/(end-start)
-            interpolated_baseline = sub_spectrum[0]*(1-i) + sub_spectrum[-1]*i
+            start = x-left
+            end = x+right
 
             self.plot(
-                title="Baseline",
-                color="orange",
-                x=[start, end],
-                y=[sub_spectrum[0], sub_spectrum[-1]],
+                title="x",
+                color="red",
+                x=[x, x],
+                y=[min(spectrum), max(spectrum)],
             )
 
-        peak = spectrum[self.to_pixel(x)]
+            self.plot(
+                title="Range",
+                color="blue",
+                x=[start, start],
+                y=[min(spectrum), max(spectrum)],
+            )
+            self.plot(
+                color="blue",
+                x=[end, end],
+                y=[min(spectrum), max(spectrum)],
+            )
 
-        #peak_region = spectrum[left_end_pixel:right_start_pixel]
-        #peak_region_subtracted = [x-interpolated_baseline for x in peak_region]
-        #area = np.trapz(peak_region_subtracted, self.get_axis()[left_end_pixel:right_start_pixel])
-        
-        format_int = lambda i: f"{int(i):,}"
+            start_pixel = self.to_pixel(start)
+            end_pixel = self.to_pixel(end)
 
-        self.table = pd.DataFrame(
-            [ format_int(interpolated_baseline), format_int(peak), format_int(peak - interpolated_baseline), "--" ],
-            index = ["Baseline", "Original Peak", "Peak (baseline subtracted)", "Peak Area (baseline subtracted)"]
-        ).T
+            sub_spectrum = list(spectrum[start_pixel:end_pixel+1])
+            if sub_spectrum:
 
-        self.metadata["Baseline"] = interpolated_baseline
-        self.metadata["OriginalPeak"] = peak
-        self.metadata["PeakBaselineSubtracted"] = peak - interpolated_baseline
-        self.metadata["Area"] = "--"
+                i = (x-start)/(end-start)
+                interpolated_baseline = sub_spectrum[0]*(1-i) + sub_spectrum[-1]*i
 
-        # keep input parameters in metadata
-        self.metadata["Left"] = self.get_widget_from_name("Left").value()
-        self.metadata["Right"] = self.get_widget_from_name("Right").value()
-        self.metadata["x"] = self.get_widget_from_name("x").value()
+                self.plot(
+                    title="Baseline",
+                    color="orange",
+                    x=[start, end],
+                    y=[sub_spectrum[0], sub_spectrum[-1]],
+                )
+
+            peak = spectrum[self.to_pixel(x)]
+
+        ##peak_region = spectrum[left_end_pixel:right_start_pixel]
+        ##peak_region_subtracted = [x-interpolated_baseline for x in peak_region]
+        ##area = np.trapz(peak_region_subtracted, self.get_axis()[left_end_pixel:right_start_pixel])
+        #
+        #format_int = lambda i: f"{int(i):,}"
+
+        #self.table = pd.DataFrame(
+        #    [ format_int(interpolated_baseline), format_int(peak), format_int(peak - interpolated_baseline), "--" ],
+        #    index = ["Baseline", "Original Peak", "Peak (baseline subtracted)", "Peak Area (baseline subtracted)"]
+        #).T
+
+        #self.metadata["Baseline"] = interpolated_baseline
+        #self.metadata["OriginalPeak"] = peak
+        #self.metadata["PeakBaselineSubtracted"] = peak - interpolated_baseline
+        #self.metadata["Area"] = "--"
+
+        ## keep input parameters in metadata
+        #self.metadata["Left"] = self.get_widget_from_name("Left").value()
+        #self.metadata["Right"] = self.get_widget_from_name("Right").value()
+        #self.metadata["x"] = self.get_widget_from_name("x").value()
