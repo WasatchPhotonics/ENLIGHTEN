96686d003e826d67aaea9293e028419c4ecd95a6
Mark Zieg <mark@zieg.com>
2023-07-05 23:22:39 -0400

    convert Cursor to ctl, extract AxisConverter

--- enlighten/scope/Cursor.py
+++ enlighten/scope/Cursor.py
@@ -9,67 +9,41 @@ from enlighten.ScrollStealFilter import ScrollStealFilter
 
 log = logging.getLogger(__name__)
 
-class Cursor(object):
-    """
-    Encapsulates the main "scope capture" cursor (vertical red line) on the
-    scope graph.  Note that StatusBar will register as an observer.
-    """
-    def __init__(self,
-            button_dn,
-            button_up,
-            cb_enable,
-            ds_value,
-            graph):
+class AxisConverter:
+    """ Move to common.AxesHelper? """
 
-        self.button_dn       = button_dn
-        self.button_up       = button_up
-        self.cb_enable       = cb_enable
-        self.ds_value        = ds_value
-        self.graph           = graph
-
-        self.multispec       = None
-        self.current_percent = None
-        self.center()
-
-        self.observers = []
-
-        # place a movable vertical line on the scope graph
-        self.cursor = pyqtgraph.InfiniteLine(movable=True, pen=self.graph.gui.make_pen(widget="scope_cursor"))
-        self.cursor.setVisible(False)
-        self.graph.add_item(self.cursor)
-
-        # bindings
-        self.cursor              .sigPositionChanged     .connect(self.moved_callback)
-        self.cb_enable           .stateChanged           .connect(self.enable_callback)
-        self.ds_value            .valueChanged           .connect(self.cursor.setValue)
-        self.ds_value                                    .installEventFilter(ScrollStealFilter(self.ds_value))
-        self.button_up           .clicked                .connect(self.up_callback)
-        self.button_dn           .clicked                .connect(self.dn_callback)
-
-        self.cb_enable.setChecked(False)
-
-        # add Cursor to Graph
-        self.graph.cursor = self
+    def __init__(self, ctl):
+        self.ctl = ctl
 
         self.px_to_wavelen = lambda x, spec: wasatch_utils.pixel_to_wavelength(x, spec.settings.eeprom.wavelength_coeffs)
         self.wavelen_to_wavenum = lambda x, spec: wasatch_utils.wavelength_to_wavenumber(x, spec.settings.eeprom.excitation_nm_float)
         self.wavenum_to_wavelen = lambda x, spec: wasatch_utils.wavenumber_to_wavelength(spec.settings.eeprom.excitation_nm_float, x)
-        # noticed you could walk the cursor for px_to_wavenum, 
-        # think it's an off by 1 error due to the 2 searchsorted
-        self.px_to_wavenum = lambda x, spec: wasatch_utils.wavelength_to_wavenumber(self.px_to_wavelen(x-1, spec), spec.settings.eeprom.excitation_nm_float)
-        self.conversions = {
-            (common.Axes.PIXELS,common.Axes.WAVELENGTHS): self.px_to_wavelen,
-            (common.Axes.PIXELS,common.Axes.WAVENUMBERS): self.px_to_wavenum,
-            (common.Axes.WAVELENGTHS,common.Axes.PIXELS): self.wavelen_to_pixels,
-            (common.Axes.WAVELENGTHS,common.Axes.WAVENUMBERS): self.wavelen_to_wavenum,
-            (common.Axes.WAVENUMBERS,common.Axes.PIXELS): self.wavenum_to_pixels,
-            (common.Axes.WAVENUMBERS,common.Axes.WAVELENGTHS): self.wavenum_to_wavelen,
-            }
 
+        # MZ: haven't thought through what this means:
+        # "noticed you could walk the cursor for px_to_wavenum, think it's an off by 1 error due to the 2 searchsorted"
+        self.px_to_wavenum = lambda x, spec: wasatch_utils.wavelength_to_wavenumber(self.px_to_wavelen(x-1, spec), spec.settings.eeprom.excitation_nm_float)
+
+        self.conversions = {
+            (common.Axes.PIXELS,      common.Axes.WAVELENGTHS): self.px_to_wavelen,
+            (common.Axes.PIXELS,      common.Axes.WAVENUMBERS): self.px_to_wavenum,
+            (common.Axes.WAVELENGTHS, common.Axes.PIXELS):      self.wavelen_to_pixels,
+            (common.Axes.WAVELENGTHS, common.Axes.WAVENUMBERS): self.wavelen_to_wavenum,
+            (common.Axes.WAVENUMBERS, common.Axes.PIXELS):      self.wavenum_to_pixels,
+            (common.Axes.WAVENUMBERS, common.Axes.WAVELENGTHS): self.wavenum_to_wavelen,
+        }
+
+    def convert(self, spec, old_axis, new_axis, x):
+        conversion_func = self.conversions.get((old_axis, new_axis), None)
+        if conversion_func is None:
+            log.debug(f"conversion func is none so not updating x")
+            return
+
+        new_x = conversion_func(x, spec)
+        return new_x
 
     def wavenum_to_pixels(self, x, spec):
         log.debug(f"wave num to pixels called")
-        specs = self.multispec.get_spectrometers()
+        specs = self.ctl.multispec.get_spectrometers()
         for spec in specs:
             wavenums = spec.settings.wavenumbers
             log.debug(f"checking spec with wavenums {wavenums[0]} and {wavenums[-1]}")
@@ -80,7 +54,7 @@ class Cursor(object):
 
     def wavelen_to_pixels(self, x, spec):
         log.debug(f"wave len to pixels called for value {x}")
-        specs = self.multispec.get_spectrometers()
+        specs = self.ctl.multispec.get_spectrometers()
         for spec in specs:
             wavelengths = spec.settings.wavelengths
             log.debug(f"checking spec with wavelengths {wavelengths[0]} and {wavelengths[-1]}")
@@ -88,6 +62,46 @@ class Cursor(object):
                 x = np.searchsorted(wavelengths, x, side="right")
                 log.debug(f"new x value is {x}")
                 return x
+
+
+class Cursor:
+
+    """
+    Encapsulates the main Graph x-axis cursor (vertical red line).
+    Note that StatusBar will register as an observer.
+    """
+    def __init__(self, ctl):
+        self.ctl = ctl
+
+        sfu = self.ctl.form.ui
+        self.cb_enable = sfu.checkBox_cursor_scope_enabled
+        self.ds_value  = sfu.doubleSpinBox_cursor_scope
+
+        self.current_percent = None
+        self.center()
+
+        self.observers = []
+
+        self.converter = AxisConverter(ctl)
+
+        # place a movable vertical line on the scope graph
+        self.cursor = pyqtgraph.InfiniteLine(movable=True, pen=self.ctl.gui.make_pen(widget="scope_cursor"))
+        self.cursor.setVisible(False)
+        self.ctl.graph.add_item(self.cursor)
+
+        # bindings
+        self.cursor              .sigPositionChanged     .connect(self.moved_callback)
+        self.cb_enable           .stateChanged           .connect(self.enable_callback)
+        self.ds_value            .valueChanged           .connect(self.cursor.setValue)
+        self.ds_value                                    .installEventFilter(ScrollStealFilter(self.ds_value))
+        sfu.pushButton_cursor_up .clicked                .connect(self.up_callback)
+        sfu.pushButton_cursor_dn .clicked                .connect(self.dn_callback)
+
+        self.cb_enable.setChecked(False)
+
+        # add Cursor to Graph
+        self.ctl.graph.cursor = self
+        self.ctl.graph.register_observer("change_axis", self.change_axis_callback)
 
     def register_observer(self, callback):
         self.observers.append(callback)
@@ -100,13 +114,6 @@ class Cursor(object):
 
     def is_enabled(self):
         return self.cb_enable.isChecked()
-
-    def set_value(self, value):
-        self.cursor.setValue(value)
-
-    def set_suffix(self, suffix):
-        log.debug(f"set suffix to {suffix}")
-        self.ds_value.setSuffix(suffix)
 
     def set_range(self, array=None):
         if array is not None:
@@ -127,31 +134,46 @@ class Cursor(object):
         util.decr_spinbox(self.ds_value)
         self.update()
 
+    def change_axis_callback(self, old_axis, new_axis):
+        """ The user changed the current x-axis on the Graph """
+
+        self.convert_location(old_axis, new_axis)
+
+        x_axis = self.ctl.generate_x_axis()
+
+        suffix = " %s" % common.AxesHelper.get_suffix(new_axis) # note space
+        log.debug(f"setting suffix to {suffix} (enum {new_axis})")
+        self.ds_value.setSuffix(suffix)
+
+        if x_axis is not None:
+            self.set_range(array=x_axis)
+
     def convert_location(self, old_axis, new_axis):
         log.debug(f"convert location from {old_axis} to {new_axis}")
-        spec = self.multispec.current_spectrometer()
+        spec = self.ctl.multispec.current_spectrometer()
         if spec is None:
             log.debug(f"spec is none so not updating x")
             return
-        x = self.cursor.getXPos()
 
-        conversion_func = self.conversions.get((old_axis,new_axis), None)
-        if conversion_func is None:
-            log.debug(f"conversion func is none so not updating x")
+        new_x = self.converter.convert(
+            spec     = spec, 
+            old_axis = old_axis, 
+            new_axis = new_axis, 
+            x        = self.cursor.getXPos())
+        if new_x is None:
             return
 
-        new_x = conversion_func(x, spec)
-        self.set_value(new_x)
+        self.cursor.setValue(new_x)
         self.update()
     
     def update(self):
         if not self.cb_enable.isChecked():
             return
 
-        if self.multispec is None:
+        if self.ctl.multispec is None:
             return
 
-        spec = self.multispec.current_spectrometer()
+        spec = self.ctl.multispec.current_spectrometer()
         if spec is None:
             return
 
@@ -181,19 +203,20 @@ class Cursor(object):
             log.error("Error updating cursor", exc_info=1)
 
     def enable_callback(self):
+        sfu = self.ctl.form.ui
         enabled = self.cb_enable.isChecked()
         log.debug("enable_callback: enabled = %s", enabled)
 
         self.ds_value       .setEnabled(enabled)
-        self.button_up      .setEnabled(enabled)
-        self.button_dn      .setEnabled(enabled)
+        sfu.pushButton_cursor_up.setEnabled(enabled)
+        sfu.pushButton_cursor_dn.setEnabled(enabled)
         self.cursor         .setVisible(enabled)
 
         if enabled and self.is_outside_range():
             self.center()
 
     def moved_callback(self, pos):
-        x_axis = self.graph.generate_x_axis() # assume selected spectrometer
+        x_axis = self.ctl.graph.generate_x_axis() # assume selected spectrometer
         log.debug(f"cursor moved callback x_axis len is {len(x_axis)}")
         if x_axis is None:
             log.error("moved_callback: no x_axis?!")
@@ -219,7 +242,7 @@ class Cursor(object):
         self.update()
 
     def move_perc(self, perc):
-        x_axis = self.graph.generate_x_axis() # assume selected
+        x_axis = self.ctl.graph.generate_x_axis() # assume selected
         if x_axis is None:
             return
 
@@ -229,7 +252,7 @@ class Cursor(object):
         log.debug("move_perc: x = %.2f (%.2f%%)", x_value, perc)
 
     def is_outside_range(self):
-        x_axis = self.graph.generate_x_axis() # assume selected
+        x_axis = self.ctl.graph.generate_x_axis() # assume selected
         if x_axis is None:
             return
 
@@ -237,7 +260,7 @@ class Cursor(object):
         return value < x_axis[0] or value > x_axis[-1]
 
     def center(self):
-        x_axis = self.graph.generate_x_axis() # assume selected
+        x_axis = self.ctl.graph.generate_x_axis() # assume selected
         if x_axis is None:
             return
 
@@ -246,3 +269,4 @@ class Cursor(object):
         midpoint = x_axis[0] + (x_axis[-1] - x_axis[0]) / 2
         self.cursor.setValue(midpoint)
         log.debug("centering: midpoint of (%.2f, %.2f) is %.2f", x_axis[0], x_axis[-1], midpoint)
+
