14736f5e0f80a3109a82b1cf7ba6ce31cdbbf642
Samie Bee <sbee@wasatchphotonics.com>
2023-08-22 08:57:50 -0400

    Fix LocalBaseline.py Move `import logging` into EnlightenPlugin.py

--- pluginExamples/Analysis/LocalBaseline.py
+++ pluginExamples/Analysis/LocalBaseline.py
@@ -1,12 +1,9 @@
 import pandas as pd
 import numpy as np
-import logging
 
 import time
 
 from EnlightenPlugin import *
-
-log = logging.getLogger(__name__)
 
 class LocalBaseline(EnlightenPluginBase):
 
