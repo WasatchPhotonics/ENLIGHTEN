39174438c892e2db91dfa724a6311a1b6eadb6c3
Mark Zieg <mzieg@wasatchphotonics.com>
2024-01-15 14:40:55 -0500

    remove env vars for more authentic test env

--- run.bat
+++ run.bat
@@ -1,9 +1,2 @@
 @echo off
-
-REM This is for DOS Command Prompt development environments
-
-REM Note be sure to do this, or run with -X utf8 
-set PYTHONUTF8=1
-set QT_AUTO_SCREEN_SCALE_FACTOR=1
-
-python scripts\Enlighten.py --log-append False --log-level debug 1>enlighten.out 2>enlighten.err
+python -X utf8 scripts\Enlighten.py --log-append False --log-level debug 1>enlighten.out 2>enlighten.err
