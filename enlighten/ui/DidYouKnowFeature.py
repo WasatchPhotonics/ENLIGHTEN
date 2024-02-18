f0a76ffa42be8baeec0a1112de19ae2c7ba8647f
Mark Zieg <mzieg@wasatchphotonics.com>
2024-02-18 17:41:57 -0500

    DYK

--- enlighten/ui/DidYouKnowFeature.py
+++ enlighten/ui/DidYouKnowFeature.py
@@ -30,18 +30,6 @@ class DidYouKnowFeature:
 
     It is unrelated to the GuideFeature, which displays contextual tips on the 
     Marquee during measurement operations.
-
-    @verbatim
-     ____________________________________________
-    | WP)    Did You Know...?                [X] |
-    |                                            |
-    | The Tip Title                   #########  |
-    |                                 #  Tip  #  |
-    | The tip text goes here...       # Image #  |
-    |                                 #########  |
-    | [X] Show at start   [Next Tip]    Tip 1/5  |
-    |____________________________________________|
-    @endverbatim
     """
     def create_tips(self):
         self.tips = []
@@ -64,7 +52,7 @@ class DidYouKnowFeature:
             <p>Use template macros like <span style="font-family:monospace; color:yellow">{integration_time_ms}ms 
             {gain_db}dB {scans_to_average}avg</span> as the default Clipboard labels or filenames of new measurements.</p>
             <p>Virtually any ENLIGHTEN variable name or property can be referenced. See the manual for a list of supported macros.</p>""")
-        tip("Wavenumber Correction", "wavenumber_correction", """
+        tip("Wavenumber Correction", "wavenumber_correction2", """
             <p>Even the most assiduously calibrated laser or spectrometer can 
                experience minor shifts in wavelength due to ambient changes in 
                environment.</p>
@@ -88,33 +76,151 @@ class DidYouKnowFeature:
                samples, units, operators and time. Read our full <a 
                href="https://wasatchphotonics.com/technologies/reproducible-raman-measurements/">Tech
                Note: Reproducible Raman Measurements</a> for additional information.</p>""")
+        tip("Laser Wavelength Precision", "laser_wavelength", """
+            <p>Wasatch spectrometers with an integrated laser include a factory-calibrated 
+               laser excitation wavelength stored in the EEPROM to picometer precision.
+               However, if you are using external lasers, you may need to tell ENLIGHTEN
+               the exact wavelength of your laser to ensure the wavenumber axis is computed
+               accurately. You can do this in Expert mode, which adds a "Wavenumber" control
+               to the Laser Control panel in 0.001nm precision.</p>""")
+        tip("Audible Laser Warning", "ear", """
+            <p>Wasatch spectrometers and ENLIGHTEN™ softwae try hard to make it extremely
+               visible when your laser is firing: there is a red LED on the spectrometer
+               housing, ENLIGHTEN™'s two Laser Enable buttons both turn red, and the "Light 
+               Source" virtual LED / Status Indicator in the lower-right corner turns a 
+               warning orange.</p>
+            <p>However, sometimes your eyes are elsewhere while managing a measurement, and
+               that's why we added an optional audible indicator as an added level of safety
+               for those who wish it. Use the Settings view to opt-in to Sound Effects if
+               you prefer to hear that comforting warble to let you know when you're emitting.</p>""")
+        tip("Copy Graph to Excel", "clipboard", """
+            <p>Most graphs in ENLIGHTEN™, including the main Scope, have a "clipboard" 📋 icon
+               in the upper corner. You can tap that at any time to copy all data from the graph
+               to the computer's system clipboard for copy-pasting between applications. The
+               copied data is tab-delimited, so can be pasted directly into Microsoft Excel for
+               quick processing and charting. If multiple traces are visible on the graph, each
+               are included in the clipboard output as separate columns.</p>""")
+        tip("Copy EEPROM to Excel", "clipboard", """
+            <p>If you're juggling multiple spectrometers, it can be important to track the 
+               settings, configuration and calibration of each. The Hardware view in ENLIGHTEN™
+               has a little 📋 clipboard icon atop the EEPROM contents, and clicking that will
+               copy the entire table to your computer's system clipboard for copy-pasting into
+               Excel, Notepad or other programs.</p>""")
+        tip("Selective Export", None, """
+            <p>When Exporting the ENLIGHTEN™ Clipboard of saved measurements to a "wide" CSV 
+               file, you can specify whether you wish to export the entire Clipboard, or just 
+               the measurements currently graphed on-screen.</p>""")
+        tip("Right-Click Graph", "chart_options", """
+            <p>The main Scope graph in ENLIGHTEN™ -- every graph in the program, for that matter --
+               can be customized by right-clicking.</p>""")
+        tip("Pan and Zoom on Trackpads", None, """
+            <p>All ENLIGHTEN™ graphs are easy enough to pan and zoom using a mouse while holding
+               the right-click button. But what if you're on a laptop with a trackpad?</p>
+            <p>Fortunately, you can still zoom and stretch your graph in two dimensions
+               using trackpad gestures. Place the cursor over one of the graph axes, then
+               control-drag (or two-finger drag) along the axis. The graph should zoom in or out
+               in the indicated dimension, allowing you to see just the peak features you care
+               about regardless of computing platform.</p>""")
+        tip("Batch Collection 'Explain This'", None, """
+            <p>Batch Collection settings can get pretty complex -- it's a flexible feature, 
+               with many configurable delays and options, and sometimes it's hard to remember
+               exactly what each field does and how that will affect the series of measurements.</p>
+            <p>There's a link on the Batch Collection setup panel called "Explain This," and if
+               you mouse-over it, it will display a tooltip explaining, in plain narrative prose,
+               how the resulting measurement will occur, with each step tied back to the settings
+               you entered.</p>""")
+        tip("Status Bar Fields", "status_bar_menu", """
+            <p>Below the main Scope graph is a Status Bar with key summary fields for the spectra 
+               and spectrometer displayed: peak intensity, detector temperature, cursor value, etc.</p>
+            <p>You can customize which fields are displayed using the "..." menu at the right end 
+               of the Status Bar. This will show additional fields not included by default, and let
+               you disable fields you don't need or use.</p>""")
+        tip("Moveable Legend", None, """
+            <p>The main Scope graph has a legend in the upper-left corner labeling each displayed
+               graph trace. You can move that around the graph by dragging with a mouse, to avoid
+               overwriting key peaks and spectral features.</p>""")
+        tip("Resizable Clipboard and Control Palette", "screen_handles", """
+            <p>We try to keep ENLIGHTEN™ usable on smaller screens and tablets, but there's so much
+               detail we love to show! One way you can claw-back some screen real-estate is by grabbing
+               invisible "handles" to the left and right of the Scope graph, and shrinking or growing the
+               scrollable columns holding the Clipboard and Control Palette.</p>""")
+        tip("Thermo Galactic SPC", "galactic", """
+             <p>ENLIGHTEN™ can read and write spectra in the Thermo Galactic SPC format, 
+                used by Thermo Scientific GRAMS and other popular spectroscopy software.</p>""")
+        tip("Boxcar Smoothing", None, """
+             <p>Boxcar smoothing applies a lightweight spatial averaging convolution
+                to remove high-frequency noise from your spectrum, at the cost of
+                slightly reduced peak intensity and optical resolution (increased
+                effective FWHM).</p>
+             <p>It can be useful to quickly make spectra "prettier" and improve 
+                "apparent SNR" for human viewers, but is a lossy transformation 
+                which rarely improves fundamental data quality. It can be useful
+                in some types of peak-finding, by eliminating low-magnitude noise
+                peaks.</p>
+             <p>Note that boxcar is applied in pixel space, and does not account for the 
+                non-linearity inherent in calibrated wavelength or wavenumber axes.</p>
+             <p>Typical values may be 1 or 2 for Raman, or up to 10 for non-Raman
+                with broad spectral features.</p>""")
+        tip("Scan Averaging", None, """
+             <p>Scan averaging is one of the simplest yet most effective things 
+                you can do to increase Signal-to-Noise Ratio (SNR).</p>
+             <p>As boxcar averages over <i>space</i>, scan averaging averages over <i>time</i>,
+                averaging several samples together to reduce high-frequency noise
+                and generate authentically smoothed spectra without compromising peak 
+                intensity or optical resolution (but at the cost of longer measurement
+                operations and laser firing time).</p>
+             <p>Setting averaging to 5 is a quick way to get a measurable boost in
+                effective signal. However, as signal is measured on a logarithmic
+                scale, you basically need to jump to 25 spectra to get the next 
+                noticable improvement in quality.</p>""")
+        tip("Baseline Correction", None, """
+             <p>Baseline correction allows you to select one of several 3rd-party
+                open-source algorithms to perform an automated "baseline subtraction"
+                from your spectra. Essentially this can be used as a simple form of
+                fluorescence removal.</p>
+             <p>Although multiple algorithms are provided for your experimentation,
+                Wasatch recommends AirPLS for use with Raman spectroscopy.</p>
+             <p>You can optionally check the "show curve" option to see the 
+                computed baseline as generated by each of the different 
+                baseline algorithms.</p>
+             <p>If you have a better (open-source) baseline-correction algorithm 
+                you'd like to see supported in ENLIGHTEN, please contact us!</p>""")
+        tip("Raman Intensity Correction", "raman_intensity_correction", """
+             <p>Raman Intensity Correction uses an EEPROM-stored calibration, 
+                generated in the factory with NIST SRM standards, to correct 
+                intensity (y-axis) on Raman spectra. This is sometimes just 
+                called "SRM correction".</p>
+            <p>Raman Intensity Correction is one of the most important steps you can
+               take to maximize repeatability in Raman signal across instruments, time
+               and operators. For more information, read our <a
+               href="https://wasatchphotonics.com/technologies/reproducible-raman-measurements/">Tech
+               Note"</a> on repeatable Raman measurements.</p>
+             <p>ENLIGHTEN™ only allows you enable Raman Intensity Correction when
+                in Raman or Expert mode, as it is only applicable to Raman spectra.
+                Raman Intensity Correction also requires a dark spectrum to be stored.
+                Therefore, when Raman Intensity Correction is enabled, you cannot
+                clear your dark; you must first disable Raman Intensity Correction
+                before clearing and refreshing your dark, then re-enabling it after.</p>
+             <p>Finally, Raman Intensity Correction requires that the horizontal
+                ROI be enabled. This is because NIST SRM standards are only certified
+                within a given spectral range, and it would be invalid to extrapolate
+                the correction factors outside the configured ROI.</p>
+             <p>As a simple visual "sanity-check" of the correction factors generated
+                from the SRM calibration coefficients, you can enable a normalized 
+                graph trace indicating how the calibration changes over the detector.
+                Typically, this will resemble a "U"-shaped parabola, with higher
+                scaling factors at the edges of the detector, and curving lower
+                toward the center.</p>""")
 
-        # @todo
-        # SRM
         # plugins
         # startup plugin
-        # Exact laser wavelength
-        # Right-scroll in graph axes
-        # Right-click graph
-        # Copy spectra to Excel
-        # Copy EEPROM to Excel
-        # Selective Export
         # Edit ROI (lock axes)
-        # Laser Sound Effects
-        # Batch Collection Explain This
-        # Choose StatusBar fields
-        # read/write SPC
         # Themes, Light Mode
         # RamanLines
         # Stats, StatsBuffer
         # EmissionLines
         # Horizontal ROI
-        # Scan Averaging
-        # Boxcar
-        # Baseline Correction
-        # Drag Legend
         # Save/Export JSON
-        # Resize Clipboard and Control Palette
         # Raman Mode
 
         # present tips in random order each time
@@ -140,6 +246,12 @@ class DidYouKnowFeature:
         if self.tips:
             self.update_dialog()
             self.dialog.exec()
+
+    def prev_callback(self):
+        self.index -= 1
+        if self.index < 0:
+            self.index = len(self.tips) - 1
+        self.update_dialog()
 
     def next_callback(self):
         self.index = (self.index + 1) % len(self.tips)
@@ -173,6 +285,20 @@ class DidYouKnowFeature:
             log.error(f"set_pixmap: unknown resource {pathname}")
 
     def create_dialog(self):
+        """
+        @verbatim
+         ____________________________________________
+        | WP)    Did You Know...?                [X] |
+        |                                            |
+        | The Tip Title                   #########  |
+        |                                 #  Tip  #  |
+        | The tip text goes here...       # Image #  |
+        |                                 #########  |
+        | [X] Show at start   [Next Tip]    Tip 1/5  |
+        |____________________________________________|
+        @endverbatim
+        """
+
         self.dialog = QDialog(parent=self.ctl.form)
         self.dialog.setModal(True)
         self.dialog.setWindowTitle("Did You Know...?")
@@ -188,11 +314,11 @@ class DidYouKnowFeature:
         lb_dyk = QLabel("Did You Know...?", parent=self.dialog)
         lb_dyk.setStyleSheet("font-size: 24px; font-weight: bold")
 
-        hb1_w = QWidget(parent=self.dialog)
-        hb1 = QHBoxLayout(hb1_w)
-        hb1.addWidget(lb_wp)
-        hb1.addWidget(lb_dyk)
-        hb1.addStretch()
+        hbox1_w = QWidget(parent=self.dialog)
+        hbox1 = QHBoxLayout(hbox1_w)
+        hbox1.addWidget(lb_wp)
+        hbox1.addWidget(lb_dyk)
+        hbox1.addStretch()
 
         ########################################################################
         # Middle Row
@@ -206,18 +332,18 @@ class DidYouKnowFeature:
         self.tb_text.setOpenExternalLinks(True)
         self.tb_text.document().setDefaultStyleSheet("a { color: #27c0a1 }") 
 
-        vb_title_text_w = QWidget(parent=self.dialog)
-        vb_title_text = QVBoxLayout(vb_title_text_w)
-        vb_title_text.addWidget(self.lb_title)
-        vb_title_text.addWidget(self.tb_text)
+        vbox_left_w = QWidget(parent=self.dialog)
+        vbox_left = QVBoxLayout(vbox_left_w)
+        vbox_left.addWidget(self.lb_title)
+        vbox_left.addWidget(self.tb_text)
 
         # right column
         self.lb_image = QLabel("placeholder", parent=self.dialog)
 
-        hb2_w = QWidget(parent=self.dialog)
-        hb2 = QHBoxLayout(hb2_w)
-        hb2.addWidget(vb_title_text_w)
-        hb2.addWidget(self.lb_image)
+        hbox2_w = QWidget(parent=self.dialog)
+        hbox2 = QHBoxLayout(hbox2_w)
+        hbox2.addWidget(vbox_left_w)
+        hbox2.addWidget(self.lb_image)
 
         ########################################################################
         # Bottom Row
@@ -227,21 +353,28 @@ class DidYouKnowFeature:
         self.cb_show_at_startup.setChecked(True)
         self.cb_show_at_startup.stateChanged.connect(self.show_at_startup_callback)
 
+        bt_prev = QPushButton(parent=self.dialog)
+        bt_prev.setText("Prev")
+        bt_prev.clicked.connect(self.prev_callback)
+        # bt_prev.setFixedSize(QtCore.QSize(50, 30))
+
         bt_next = QPushButton(parent=self.dialog)
-        bt_next.setText("Next Tip")
+        bt_next.setText("Next")
         bt_next.clicked.connect(self.next_callback)
+        # bt_next.setFixedSize(QtCore.QSize(50, 30))
 
         self.lb_x_of_y = QLabel("Tip X/Y", parent=self.dialog)
 
-        hb3_w = QWidget(parent=self.dialog)
-        hb3 = QHBoxLayout(hb3_w)
-        hb3.addWidget(self.cb_show_at_startup)
-        hb3.addStretch()
-        hb3.addWidget(bt_next)
-        hb3.addStretch()
-        hb3.addWidget(self.lb_x_of_y)
+        hbox3_w = QWidget(parent=self.dialog)
+        hbox3 = QHBoxLayout(hbox3_w)
+        hbox3.addWidget(self.cb_show_at_startup)
+        hbox3.addStretch()
+        hbox3.addWidget(bt_prev)
+        hbox3.addWidget(bt_next)
+        hbox3.addStretch()
+        hbox3.addWidget(self.lb_x_of_y)
 
-        vb_all = QVBoxLayout(self.dialog)
-        vb_all.addWidget(hb1_w)
-        vb_all.addWidget(hb2_w)
-        vb_all.addWidget(hb3_w)
+        vbox_all = QVBoxLayout(self.dialog)
+        vbox_all.addWidget(hbox1_w)
+        vbox_all.addWidget(hbox2_w)
+        vbox_all.addWidget(hbox3_w)
