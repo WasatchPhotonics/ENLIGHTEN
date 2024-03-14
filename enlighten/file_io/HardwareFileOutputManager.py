fbf8ef99dc8448ed1cbe258e330a7c414250e277
Mark Zieg <mzieg@wasatchphotonics.com>
2024-03-14 18:05:08 -0400

    docs

--- enlighten/file_io/HardwareFileOutputManager.py
+++ enlighten/file_io/HardwareFileOutputManager.py
@@ -29,6 +29,7 @@ class HardwareFileOutputManager:
             self.disable_output()
 
     def register_feature(self, feature_obj):
+        """ @todo rename register_observer """
         self.features.append(feature_obj)
 
     def enable_output(self):
