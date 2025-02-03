3d7d9ebfa198fa95449366b637cacbaa90791f9c
Mark Zieg <mzieg@wasatchphotonics.com>
2025-02-03 16:14:43 -0500

    added Quick Wipe, metrics

--- plugins/Analysis/Tail.py
+++ plugins/Analysis/Tail.py
@@ -1,4 +1,6 @@
 import logging
+from datetime import datetime
+
 from EnlightenPlugin import EnlightenPluginBase
 
 log = logging.getLogger(__name__)
@@ -19,14 +21,38 @@ class Tail(EnlightenPluginBase):
         self.block_enlighten = True 
         self.curves = []
         self.data = []
+        self.colors = [ '#0097ff', 
+                        '#0fd207', 
+                        '#e9ff06', 
+                        '#ffb507', 
+                        '#ff4d21' ]
+        self.frames = 0
+        self.start_time = datetime.now()
 
-        self.field(name="Length", direction="input", datatype=int, minimum=1, maximum=20, initial=5, tooltip="Number of spectra in tail")
-        self.field(name="Clear", datatype="button", callback=self.reset, tooltip="Clear history")
+        # for quick wipe
+        self.cache_integ_time = 10
+        self.wipe_enabled = False
+        self.wiping = False
+
+        # self.field(name="Length", direction="input", datatype=int, minimum=1, maximum=20, initial=5, tooltip="Number of spectra in tail")
+
+        self.field(name="Reset", datatype="button", callback=self.reset, tooltip="Clear history")
+        self.field(name="Frames", datatype=int, tooltip="Frames since reset")
+        self.field(name="Time", datatype=str, tooltip="Time since reset")
+
+        self.field(name="Quick Wipe", datatype=bool, direction="input", tooltip=f"Quickly wipe detector by generating throwaway reads ot minimum integration time after a reset")
+        self.field(name="Wipe Count", datatype=int , direction="input", initial=20, minimum=1, maximum=250, tooltip="How many throwaways to generate")
+
+        self.field(name="Max Mag",  datatype=float, precision=2, direction="output")
+        self.field(name="Area",     datatype=float, precision=2, direction="output")
+        self.field(name="Abs Area", datatype=float, precision=2, direction="output")
 
     def process_request(self, request):
         spec = request.spec
         spectrum = request.processed_reading.get_processed()
-        length = request.fields["Length"]
+        self.wipe_enabled = request.fields["Quick Wipe"]
+
+        length = 5 # request.fields["Length"]
 
         # if length reduced, delete extra curves
         while len(self.curves) > length:
@@ -40,6 +66,8 @@ class Tail(EnlightenPluginBase):
             name = f"{spec.settings.eeprom.serial_number}-tail-{len(self.curves)}"
             log.debug(f"adding curve {name}")
             curve = self.ctl.graph.add_curve(name=name, rehide=False, in_legend=False)
+            pen = self.ctl.gui.make_pen(widget="tail", color=self.colors[len(self.curves)])
+            curve.setPen(pen)
             self.curves.append(curve)
 
         # pop oldest history
@@ -60,6 +88,67 @@ class Tail(EnlightenPluginBase):
                 y = self.data[i][1]
             self.ctl.graph.set_data(curve, x=x, y=y)
 
+        self.update_metrics(request)
+
+        self.frames += 1
+
+        if self.wiping:
+            if self.frames > request.fields["Wipe Count"]:
+                self.stop_wipe()
+
+    def update_metrics(self, request):
+        spectrum = request.processed_reading.get_processed()
+        dark = request.processed_reading.get_dark()
+
+        self.outputs["Frames"] = self.frames
+        self.outputs["Time"] = str(datetime.now() - self.start_time)
+
+        if dark is None:
+            return
+
+        total = 0
+        abs_total = 0
+        max_mag = 0
+        for i in range(len(spectrum)):
+            mag = abs(spectrum[i])
+            total += spectrum[i]
+            abs_total += mag
+            max_mag = max(max_mag, mag)
+
+        self.outputs["Area"] = total / len(spectrum)
+        self.outputs["Abs Area"] = abs_total / len(spectrum)
+        self.outputs["Max Mag"] = max_mag
+
+
     def reset(self):
-        for i in range(len(curves)):
-            self.ctl.graph.set_data(self.curves[i], x=[], y=[])
+        # for i in range(len(curves)):
+        #     self.ctl.graph.set_data(self.curves[i], x=[], y=[])
+        self.frames = 0
+        self.start_time = datetime.now()
+        self.start_wipe()
+
+    def stop_wipe(self):
+        log.debug("trying to stop wipe")
+        if not self.wiping:
+            return
+        spec = self.ctl.multispec.current_spectrometer()
+        if spec is None:
+            return
+
+        spec.change_device_setting("integration_time_ms", self.cache_integ_time)
+        log.debug(f"finished fast wipe (count {self.frames})")
+        self.wiping = False
+
+    def start_wipe(self):
+        self.wiping = False
+        if not self.wipe_enabled:
+            return
+
+        spec = self.ctl.multispec.current_spectrometer()
+        if spec is None:
+            return
+
+        log.debug("starting fast wipe")
+        self.cache_integ_time = spec.settings.state.integration_time_ms
+        spec.change_device_setting("integration_time_ms", spec.settings.eeprom.min_integration_time_ms)
+        self.wiping = True
