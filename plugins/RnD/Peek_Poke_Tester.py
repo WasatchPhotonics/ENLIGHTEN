b7f2a0339f2221e80daad107f22ad81a4d5118bf
Christopher Grainger <cgrainger@wasatchphotonics.com>
2026-07-21 15:49:48 -0400

    Device USB made into a function, layout updated, poke automatically peeks to confirm change

--- plugins/RnD/Peek_Poke_Tester.py
+++ plugins/RnD/Peek_Poke_Tester.py
@@ -27,37 +27,22 @@ class Peek_Poke_Tester(EnlightenPluginBase):
         
         self.field(name = "Address", datatype = str, direction = "input", tooltip="values should be entered in hex (leading 0x optional)") 
         self.field(name = "Length", datatype = int, direction = "input", minimum=1, maximum=64, initial=1) 
-        self.field(name = "Value", datatype = str, direction = "output")
-        self.field(name = "Peek", datatype = "button", callback = self.run_peek)
-        
-        self.field(name = "Poke Address", datatype = str, direction = "input", tooltip = "values should be entered in hex (leading 0x optional)")
         self.field(name = "Poke Value", datatype = str, direction = "input", tooltip = "values should be entered in hex (leading 0x optional)")
+        self.field(name = "Value", datatype = str, direction = "output")
+       
+        self.field(name = "Peek", datatype = "button", callback = self.run_peek)
         self.field(name = "Poke", datatype = "button", callback = self.run_poke)
+        
+        self.usb_core_device = self.get_usb_device()
         
 #############################################
 # Peek Commands
 #############################################
     
-    def run_peek(self):               
-        ########################################################################
-        # get existing handle to usb.core.Device
-        ########################################################################
-
-        spec = self.ctl.multispec.current_spectrometer()
-        if spec is None:
-            self.ctl.marquee.error("PeekPokeTester requires spectrometer")
-            return
-
-        if not spec.settings.is_xs():
-            self.ctl.marquee.error("PeekPokeTester requires XS")
-            return
-
-        wdw = spec.device # a wasatch.WasatchDeviceWrapper
-        worker = wdw.wrapper_worker # a wasatch.WrapperWorker
-        wasatch_device = worker.connected_device # a wasatch.WasatchDevice (since we already confirmed it's an XS)
-        fid = wasatch_device.hardware # a wasatch.FeatureInterfaceDevice
-        usb_core_device = fid.device # a usb.core.Device from pyusb
-
+    def run_peek(self):
+        # Get the USB information
+        #usb_core_device = self.get_usb_device()
+        
         ########################################################################
         # get peek address and length
         ########################################################################
@@ -69,7 +54,7 @@ class Peek_Poke_Tester(EnlightenPluginBase):
         # perform the peek
         ########################################################################
 
-        data = self.get_cmd(dev=usb_core_device, cmd=0x91, value=address, length=length)
+        data = self.get_cmd(dev=self.usb_core_device, cmd=0x91, value=address, length=length)
         if data is None:
             self.ctl.marquee.error("PeekPokeTester peek returned None")
             return
@@ -119,7 +104,51 @@ class Peek_Poke_Tester(EnlightenPluginBase):
 # Poke Commands
 #############################################
 
-    def run_poke(self):
+    def run_poke(self):        
+        # Get the USB information
+        #usb_core_device = self.get_usb_device()
+        
+        ########################################################################
+        # get poke address and value
+        ########################################################################
+        
+        #poke_address = int(self.get_widget_from_name("Poke Address").text().lower().removeprefix("0x"), 16)
+        poke_address = int(self.get_widget_from_name("Address").text().lower().removeprefix("0x"), 16)
+        poke_value = [int(self.get_widget_from_name("Poke Value").text().lower().removeprefix("0x"), 16)]
+        
+        # initialize buffer from values
+        buf = [ x for x in poke_value ]
+
+        # ensure at least 8 elements in buffer
+        while len(buf) < 8:
+            buf.append(0)
+        
+        data = self.send_cmd(dev = self.usb_core_device, cmd = 0x90, value = poke_address, index = len(poke_value), buf = buf)
+        
+        self.run_peek()
+    
+    def send_cmd(self, dev, cmd, value, index, buf = None):        
+        #log.debug(f"Device: {dev}")
+        #log.debug(f"cmd: {cmd}")
+        #log.debug(f"value: {value}")
+        #log.debug(f"index: {index}")
+        #log.debug(f"length: {length}")
+        #log.debug(f"lsb_len: {lsb_len}")
+        #log.debug(f"msb_len: {msb_len}")        
+        
+        dev.ctrl_transfer(
+            HOST_TO_DEVICE, 
+            cmd, 
+            value, 
+            index, 
+            buf, 
+            Timeout_ms)
+            
+#############################################
+# USB Commands
+#############################################    
+    
+    def get_usb_device(self):
         ########################################################################
         # get existing handle to usb.core.Device
         ########################################################################
@@ -138,38 +167,4 @@ class Peek_Poke_Tester(EnlightenPluginBase):
         wasatch_device = worker.connected_device # a wasatch.WasatchDevice (since we already confirmed it's an XS)
         fid = wasatch_device.hardware # a wasatch.FeatureInterfaceDevice
         usb_core_device = fid.device # a usb.core.Device from pyusb
-        
-        
-        ########################################################################
-        # get poke address and value
-        ########################################################################
-        
-        poke_address = int(self.get_widget_from_name("Poke Address").text().lower().removeprefix("0x"), 16)
-        poke_value = [int(self.get_widget_from_name("Poke Value").text().lower().removeprefix("0x"), 16)]
-        
-        # initialize buffer from values
-        buf = [ x for x in poke_value ]
-
-        # ensure at least 8 elements in buffer
-        while len(buf) < 8:
-            buf.append(0)
-        
-        data = self.send_cmd(dev = usb_core_device, cmd = 0x90, value = poke_address, index = len(poke_value), buf = buf)
-    
-    
-    def send_cmd(self, dev, cmd, value, index, buf = None):        
-        #log.debug(f"Device: {dev}")
-        #log.debug(f"cmd: {cmd}")
-        #log.debug(f"value: {value}")
-        #log.debug(f"index: {index}")
-        #log.debug(f"length: {length}")
-        #log.debug(f"lsb_len: {lsb_len}")
-        #log.debug(f"msb_len: {msb_len}")        
-        
-        dev.ctrl_transfer(
-            HOST_TO_DEVICE, 
-            cmd, 
-            value, 
-            index, 
-            buf, 
-            Timeout_ms)
+        return usb_core_device
