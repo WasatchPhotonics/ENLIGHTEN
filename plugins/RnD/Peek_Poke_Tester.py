057fdd09831fa7c027bd5ff965cf96c29ef05709
Christopher Grainger <cgrainger@wasatchphotonics.com>
2026-07-21 09:45:25 -0400

    some updates and cleaning, still doesn't work though

--- plugins/RnD/Peek_Poke_Tester.py
+++ plugins/RnD/Peek_Poke_Tester.py
@@ -5,46 +5,35 @@ import logging
 import os
 from EnlightenPlugin import EnlightenPluginBase
 log = logging.getLogger(__name__)
-#from wasatch.FeatureIdentificationDevice import FeatureIdentificationDevice
 
 #pid = 0x4000
 dev = usb.core.find(idVendor = 0x24aa, idProduct = 0x4000)
 HOST_TO_DEVICE = 0x40
 DEVICE_TO_HOST = 0xC0
-TIMEOUT_MS = 1000  
+Timeout_ms = 1000  
 
 
 class Peek_Poke_Tester(EnlightenPluginBase):    
     
-    def get_configuration(self):        
-        self.name = "Registrar Peek"
+    def get_configuration(self):         
+        self.name = "Register Peek"
         self.process_requests = False
         
-        self.field(name = "Address", datatype = str, direction = "input")
-        self.field(name = "Length", datatype = str, direction = "input")
+        self.field(name = "Address", datatype = str, direction = "input") 
+        self.field(name = "Length", datatype = str, direction = "input") 
         self.field(name = "Value", datatype = str, direction = "output")
         self.field(name = "Peek", datatype = "button", callback = self.run_peek)
         
-        #self.run_peek()
-        
     def process_request(self, request):        
-        self.address = str(request.fields["Address"])
-        self.length = str(request.fields["Length"])
+        self.address = request.fields["Address"]
+        self.length = request.fields["Length"]
     
-    def run_peek(self):
-        values = ""
-        
-        #progname = sys.argv.pop(0)
-        
-        #data = self.get_cmd(0x91, self.address, length = self.length)
+    def run_peek(self):               
         data = self.get_cmd(address = self.address, length = self.length)
         data_hex = " ".join([f"{v:02x}" for v in data])
         log.debug(f"data: {data}")
         log.debug(f"data_hex: {data_hex}")
         log.debug(f'Result Print: {self.address} << 0x{data_hex} ({len(data)} bytes)')
-        #values = f"0x{self.address:02x} << 0x{data_hex} ({len(data)} bytes)"
-        #values = f"Result: {self.address} << 0x{data_hex} ({len(data)} bytes)"
-        #values = str(self.address) + "<< 0x" + str(data_hex) + str(len(data))
         self.outputs["Value"] = f'0x{data_hex}'
         
     def get_cmd(self, address: int, value: int = 0, index: int = 0, length: int = 64, lsb_len: int = None, msb_len: int = None):
@@ -59,11 +48,13 @@ class Peek_Poke_Tester(EnlightenPluginBase):
         log.debug(f"msb_len: {msb_len}")
         
         result = dev.ctrl_transfer(
-            DEVICE_TO_HOST, 
-            int(address, 16), 
-            value, 
-            index, 
-            length)
+            bmRequestType =     DEVICE_TO_HOST, 
+            bRequest =          int(address, 16),
+            wValue =            value, 
+            wIndex =            index, 
+            data_or_wLength =   length,
+            timeout =           Timeout_ms)
+        
         log.debug(f"Result: {result}")
 
         value = 0
@@ -77,8 +68,3 @@ class Peek_Poke_Tester(EnlightenPluginBase):
             return value
         else:
             return result
-
-    def html_list(self, name, a):
-        if len(a) == 0:
-            return ""
-        return f"{name}:<ul><li>" + "</li><li>".join(a) + "</li></ul>"
