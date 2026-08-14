75cc8481b78c110d4abb19e202dfb4d59475d544
Christopher Grainger <cgrainger@wasatchphotonics.com>
2026-08-14 15:13:46 -0400

    Mapper now scans across the samples, Auto-Raman needed still

--- plugins/RnD/Mapper.py
+++ plugins/RnD/Mapper.py
@@ -19,18 +19,24 @@ Change Log:
     - Version updated to 1.1
 - 08-04-2026    
     - Updated update_display to allow for changes in um and mm
+- ?
+    - Removed all that pesky commented code
+    - Initial startup homes to 0,0 to set initial values then moves to the approximate location of cell 1
 
 Questions:
 - should the "Home Mapper" button be removed?  if we home at startup is there ever a reason to home after?
+    - This seems more appropriate with the coming updates
 - currently completes the X-axis movement then the Y-axis movement, can/should we do both simultaneously? 
 - Should the directional controls work from the camera perspective?
 
 To Do:
-- Remove the commented code sections once confirmed it is liked that way.
 - Shortcut keys for the fine tuning directional controls?
-- Make it home to the first sample cell, will have to make a diagram of the cell names/locations
+- Make sure it updates the position properly after initial startup
 - Add a field to move to a particular cell
 - Add a button to scan and auto-raman each cell
+- Update the X-axis min/max to prevent it from hitting the frame
+- Update the Y-axis max to prevent the same
+- Figure out how to use AutoRamanFeature so we can take a sample
 """
 
 import logging
@@ -51,6 +57,11 @@ class Mapper(EnlightenPluginBase):
     ############################################################################
 
     def get_configuration(self):
+        #Cell Locations....maybe a better way?
+        self.cell_1_x = 85
+        self.cell_1_y = 203
+        self.center_distance = 5.5
+        
         self.name = f"Mapper {self.VERSION}"
 
         self.field(
@@ -151,7 +162,13 @@ class Mapper(EnlightenPluginBase):
             callback=self.home_mapper,
             tooltip="Finds the Home Position of the Mapper and Defines it as 0/0"
         )
-
+        
+        self.field(
+            name = "Map Samples",
+            datatype = "button",
+            callback = self.run_mapping,
+            tooltip = "Takes sample measurement using Auto-Raman on all samples"
+        )
 
         self.has_other_graph = False
         self.block_enlighten = True
@@ -160,15 +177,18 @@ class Mapper(EnlightenPluginBase):
         # Mapper Definitions
         #        
         
-        self.mapper = MapperArduino.Mapper()        
+        self.mapper = MapperArduino.Mapper()   
         
-        self.position_x = 0.0
-        self.position_y = 0.0
-        self.step_size = 1.0
-        self.target_x = 0.0
-        self.target_y = 0.0
-        
+        # Moves to home to set the initial location
         self.home_mapper()
+        
+        # Set the target values to the location of cell 1
+        self.target_x = self.cell_1_x
+        self.target_y = self.cell_1_y
+        
+        # Moves to the target, in this case cell 1
+        self.move_to_target()
+        self.update_display()
         
     ############################################################################
     # Mapper Functions
@@ -185,68 +205,28 @@ class Mapper(EnlightenPluginBase):
         #self.update_display()
 
 # Moving positive on the Y-axis based on the current value of step_size
-    def step_forward(self):
-        #if self.get_widget_from_name("Step Size (um)").isChecked():
-            #self.mapper.y.move_relative_um(self.step_size)
-            # update internal variables
-            #self.position_y += self.step_size
-            #self.update_display()
-        #else:
-            #self.mapper.y.move_relative_mm(self.step_size)
-            #self.position_y += self.step_size
-            #self.update_display()
-            
+    def step_forward(self):            
         self.mapper.y.move_relative_um(self.step_size)
         # update internal variables
         self.position_y += (self.step_size / 1000)
         self.update_display()
         
 # Moving negative on the Y-axis based on the current value of step_size
-    def step_back(self):
-        #if self.get_widget_from_name("Step Size (um)").isChecked():
-            #self.mapper.y.move_relative_um(-self.step_size)
-            # update internal variables
-            #self.position_y -= self.step_size
-            #self.update_display()
-        #else:
-            #self.mapper.y.move_relative_mm(-self.step_size)
-            #self.position_y -= self.step_size
-            #self.update_display()
-            
+    def step_back(self):            
         self.mapper.y.move_relative_um(-self.step_size)
         # update internal variables
         self.position_y -= (self.step_size / 1000)
         self.update_display()
 
 # Moving positive on the X-axis based on the current value of step_size 
-    def step_left(self):
-        #if self.get_widget_from_name("Step Size (um)").isChecked():
-            #self.mapper.x.move_relative_um(-self.step_size)
-            # update internal variables
-            #self.position_x -= self.step_size
-            #self.update_display()
-        #else:
-            #self.mapper.x.move_relative_mm(-self.step_size)
-            #self.position_x -= self.step_size
-            #self.update_display()
-            
+    def step_left(self):            
         self.mapper.x.move_relative_um(-self.step_size)
         # update internal variables
         self.position_x -= (self.step_size / 1000)
         self.update_display()
 
 # Moving negative on the X-axis based on the current value of step_size   
-    def step_right(self):
-        #if self.get_widget_from_name("Step Size (um)").isChecked():
-            #self.mapper.x.move_relative_um(self.step_size)
-            # update internal variables
-            #self.position_x += self.step_size
-            #self.update_display()
-        #else:
-            #self.mapper.x.move_relative_mm(self.step_size)
-            #self.position_x += self.step_size
-            #self.update_display()
-            
+    def step_right(self):            
         self.mapper.x.move_relative_um(self.step_size)
         # update internal variables
         self.position_x += (self.step_size / 1000)
@@ -287,6 +267,90 @@ class Mapper(EnlightenPluginBase):
             log.debug(f"Current X-axis position: {self.position_x}")
             self.get_widget_from_name("Y Position (mm)").setText(f"{self.position_y}")
             log.debug(f"Current Y-axis position: {self.position_y}")
+            
+    def run_mapping(self):
+        # The mapper should have been centered on cell 1 before starting
+        self.four_samples() #1 
+        self.four_samples() #2
+        self.two_samples() #3
+        self.four_samples() #4
+        self.three_samples() #5
+        self.four_samples() #6
+        self.four_samples() #7
+        self.two_samples() #8
+        self.four_samples() #9
+        self.two_samples() #10
+        self.four_samples() #11
+        self.four_samples() #12
+        self.three_samples() #13
+        self.four_samples() #14
+        self.two_samples() #15
+        self.four_samples() #16
+        self.four_samples() #17
+        self.three_samples() #18
+        self.four_samples() #19
+        self.two_samples() #20
+        self.four_samples() #21
+        self.four_samples() #22
+        self.three_samples() #23
+        self.four_samples() #24
+        self.two_samples() #25
+        self.four_samples() #26
+        self.four_samples() #27   
+    
+    # ##################################
+    # Mapping/Scanning Functions
+    # ##################################
+    #def move_down(self):
+        # This will move the mapper down 1 row, towards the motors
+        #self.target_x = self.cell_1_x
+        #self.target_y = self.position_y - self.center_distance
+        #self.move_to_target()
+    
+    def four_samples(self):
+        # Takes the next four samples
+        for i in range(3):
+            # Take an Auto-Raman Sample and save it
+            self.target_x = self.position_x + self.center_distance
+            self.move_to_target()
+            
+        # This will move the mapper down 1 row, towards the motors
+        self.target_x = self.cell_1_x
+        self.target_y = self.position_y - self.center_distance
+        self.move_to_target()
+    
+    def three_samples(self):
+        # Move over one due to the missing cell at the beginning
+        self.target_x = self.position_x + self.center_distance
+        self.move_to_target()
+        
+        # Takes the next three samples
+        for i in range(2):
+            # Take an Auto-Raman sample and save it
+            self.target_x = self.position_x + self.center_distance
+            self.move_to_target()
+    
+        # This will move the mapper down 1 row, towards the motors
+        self.target_x = self.cell_1_x
+        self.target_y = self.position_y - self.center_distance
+        self.move_to_target()
+        
+    def two_samples(self):
+        # Move over one due to the missing cell at the beginning
+        self.target_x = self.position_x + self.center_distance
+        self.move_to_target()
+        
+        # Take the next two samples
+        for i in range(1):
+            # Take an Auto-Raman sample and save it
+            self.target_x = self.position_x + self.center_distance
+            self.move_to_target()
+        
+        # This will move the mapper down 1 row, towards the motors
+        self.target_x = self.cell_1_x
+        self.target_y = self.position_y - self.center_distance
+        self.move_to_target()
+        
 
 
     ############################################################################
@@ -303,7 +367,53 @@ class Mapper(EnlightenPluginBase):
 
 
     def disconnect(self):
+        self.home_mapper()
         self.mapper.disconnect()
         super().disconnect()
         
     
+    
+    
+"""
+Cell Locations:
+
+::Back of tray::
+
+Cells: 1, 2, 3, 4
+XXXX  
+XXXX
+ XX
+ 
+XXXX
+ XXX
+XXXX
+XXXX
+ XX
+ 
+XXXX
+ XX
+ 
+XXXX
+XXXX
+ XXX
+XXXX
+ XX
+ 
+XXXX
+XXXX
+ XXX
+XXXX
+ XX
+
+XXXX
+XXXX
+ XXX
+XXXX
+ XX 
+
+XXXX
+XXXX
+
+::Front of Tray::
+::Motors::
+"""    
